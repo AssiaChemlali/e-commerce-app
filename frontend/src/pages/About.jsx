3cc8dd5b77a7f6479a9693bdf64741ba2c17b95d
AssiaChemlali <chemlali.assia@gmail.com>
2024-12-28 11:24:13 +0100

    add why choose us and newslater features

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
-
+import Heading from '../components/Heading'
+import Newslatter from '../components/Newslatter'
+import {assets} from '../assets/assets'
 const About = () => {
   return (
-    <div>
-      about
+    <div className='border-t py-10 text-center m-auto'>
+     <Heading title="ABOUT US"/>
+     <div className='flex flex-row gap-10 mt-10'>
+      <div className="w-full sm:w-1/2">
+          <img src={assets.about_img} alt=""  className='object-cover w-full h-full'/>
+      </div>
+      <div className="w-full sm:w-1/2 text-base text-gray-600 flex gap-4 flex-col text-left">
+        <p>Forever was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</p>
+         <p>
+         Since our inception, we've worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty to electronics and home essentials, we offer an extensive collection sourced from trusted brands and suppliers.
+         </p>
+         <h6 className='font-bold text-black'>Our Mission</h6>
+         <p>
+         Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.
+         </p>
+      </div>
+     </div>
+
+      <div className='my-20'>
+        <Heading title="WHY CHOOSE US" />
+        <div className='flex mt-10'>
+
+          <div className='p-16 border text-left'>
+            <h6 className='font-bold mb-5'>Quality Assurance:</h6>
+            <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+          </div>
+          <div className='p-16 border text-left'>
+            <h6 className='font-bold mb-5'>Convenience:
+            </h6>
+            <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+          </div>
+          <div className='p-16 border text-left'>
+            <h6 className='font-bold mb-5'>Exceptional Customer Service:</h6>
+            <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+          </div>
+        </div>
+      </div>
+      <Newslatter/>
+
     </div>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,11 +5,17 @@ import { useState } from 'react'
 const Login = () => {
 
   const [currentState,setCurrentState]=useState("Login")
+  function handleSubmit(e){
+    e.preventDefault()
+  }
 
   return (
     <div className='flex items-center  flex-col text-center m-auto py-20'>
       <Heading title={currentState}/>
-      <form action="" className='flex flex-col w-[90%]  gap-5 mt-5 sm:max-w-96 m-auto'>
+      <form 
+      onSubmit={handleSubmit}
+      action="" 
+      className='flex flex-col w-[90%]  gap-5 mt-5 sm:max-w-96 m-auto'>
         {
           currentState==='Sign Up' && <input type="text" name="" id=""  className='border border-black rounded py-2 px-3 w-full 'placeholder='Name'/>
         }
@@ -25,15 +31,14 @@ const Login = () => {
             ? setCurrentState('Sign Up')
             :setCurrentState('Login')
           }}>
-            {
-              currentState ==="Login" ? 'create account':'Login here'
+            { currentState ==="Login" ? 'create account':'Login here'
             }
            
             </span>
         </p>
         <button 
-        onClick={()=>setLogin(true)}
-        className='bg-black text-white py-2 px-5 uppercase text-sm '>{currentState} </button>
+  
+        className='bg-black text-white py-2 px-5 uppercase text-sm '>{currentState==='Login' ? 'Sign In':'Sign Up'} </button>
       </form>
     </div>
   )
