54a7667adbfe0dfd336a4bbbfed4442fef2a874d
AssiaChemlali <chemlali.assia@gmail.com>
2024-12-28 10:55:52 +0100

    add sign up feature

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import Heading from '../components/Heading'
 import {Link} from 'react-router-dom'
+import { useState } from 'react'
 const Login = () => {
+
+  const [currentState,setCurrentState]=useState("Login")
+
   return (
     <div className='flex items-center  flex-col text-center m-auto py-20'>
-      <Heading title=" Login"/>
-      <form action="" className='flex flex-col w-[450px]  gap-5 mt-5'>
-        
+      <Heading title={currentState}/>
+      <form action="" className='flex flex-col w-[90%]  gap-5 mt-5 sm:max-w-96 m-auto'>
+        {
+          currentState==='Sign Up' && <input type="text" name="" id=""  className='border border-black rounded py-2 px-3 w-full 'placeholder='Name'/>
+        }
+      
         <input type="email" name="" id=""  className='border border-black rounded py-2 px-3 w-full 'placeholder='Email'/>
         <input type="password" name="" id="" className='border border-black rounded py-2 px-3 w-full' placeholder='Password'/>
         <p className='gap-3 flex flex-row justify-between text-sm'>
-          <Link>Forgot Your Password?</Link>
-          <Link>Create Account</Link>
+          <button>Forgot Your Password?</button>
+          <span 
+          className='cursor-pointer capitalize'
+          onClick={()=>{
+            currentState==='Login' 
+            ? setCurrentState('Sign Up')
+            :setCurrentState('Login')
+          }}>
+            {
+              currentState ==="Login" ? 'create account':'Login here'
+            }
+           
+            </span>
         </p>
-        <button className='bg-black text-white py-2 px-5 uppercase text-sm '>Sign in </button>
+        <button 
+        onClick={()=>setLogin(true)}
+        className='bg-black text-white py-2 px-5 uppercase text-sm '>{currentState} </button>
       </form>
     </div>
   )
